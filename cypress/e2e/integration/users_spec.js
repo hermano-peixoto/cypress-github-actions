0f908bb83d69e3961aacd032e422478b5299a87a
Hermano Peixoto <hermano.peixoto@prof.inteli.edu.br>
2024-05-27 11:51:47 -0300

    Initial commit with Cypress integration

--- cypress/e2e/integration/users_spec.js
+++ cypress/e2e/integration/users_spec.js
@@ -1,6 +1,7 @@
 
 describe('Users API', () => {
     it('fetches the list of users', () => {
+        // Set up the intercept before making the request
         cy.intercept('GET', '/users', (req) => {
             req.reply([
                 { id: 1, name: 'Mocked Alice' },
@@ -9,9 +10,19 @@ describe('Users API', () => {
             ]);
         }).as('getUsers');
 
-        cy.request('/users').its('body').should('have.length', 3);
+        // Make a request to the /users endpoint
+        cy.request('/users').as('usersRequest');
+
+        // Wait for the intercept and then assert the response
         cy.wait('@getUsers').then((xhr) => {
+            expect(xhr.response.body).to.have.length(3);
             expect(xhr.response.body[0].name).to.equal('Mocked Alice');
+        });
+
+        // Additionally, assert the actual response from the request
+        cy.get('@usersRequest').then((response) => {
+            expect(response.body).to.have.length(3);
+            expect(response.body[0].name).to.equal('Mocked Alice');
         });
     });
 });
