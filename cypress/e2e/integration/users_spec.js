32afbf9d84db02a3cff9efc5703bb25afde38ff9
Hermano Peixoto <hermano.peixoto@prof.inteli.edu.br>
2024-05-27 10:43:17 -0300

    Initial commit with Cypress integration

---
+++ cypress/e2e/integration/users_spec.js
@@ -0,0 +1,16 @@
+
+describe('Users API', () => {
+    it('fetches the list of users', () => {
+        cy.intercept('GET', '/users', [
+            { id: 1, name: 'Mocked Alice' },
+            { id: 2, name: 'Mocked Bob' },
+            { id: 3, name: 'Mocked Charlie' }
+        ]).as('getUsers');
+
+        cy.visit('http://localhost:3000');
+        cy.request('/users').its('body').should('have.length', 3);
+        cy.get('@getUsers').should((xhr) => {
+            expect(xhr.response.body[0].name).to.equal('Mocked Alice');
+        });
+    });
+});
